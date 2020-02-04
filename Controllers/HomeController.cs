3d371b66dbc708b2a875248902c70b4951ad2a17
MoeinShafienia <shafienia.moein@gmail.com>
2020-02-04 18:53:56 +0330

    remove codes about 404 not found

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using System.Net;
 
 namespace Redirecting.Controllers
 {
+    [Route("{**page}")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -19,28 +20,10 @@ namespace Redirecting.Controllers
             _logger = logger;
         }
 
-        [Route("www/{DomainName=DomainName}/{Domain=Domain}")]
         public IActionResult Get(string domainName, string domain)
         {
-            string url = "https://www." + domainName + "." + domain;
-            Uri urlCheck = new Uri(url);
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlCheck);
-            request.Timeout = 15000;
-            HttpWebResponse response;
-            try
-            {
-                response = (HttpWebResponse)request.GetResponse();
-                return Redirect(url);
-            }
-            catch (Exception)    
-            {
-                return NotFound(); //could not connect to the internet (maybe) 
-            }
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            string url = Request.Path.ToString().Substring(1).Replace("/", ".");
+            return Redirect(url);
         }
     }
 }
